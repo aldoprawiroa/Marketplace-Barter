b6cc71f1f309d9271030e90fa15a35cd0d3381ab
Ibra019. <him997090@gmail.com.>
2026-04-13 11:47:25 +0700

    feat: setup array data barang mentahan

---
+++ js/data.js
@@ -0,0 +1,5 @@
+const dummyData = [
+  { id: "B01", nama: "Kamera Sony A6000", harga: 3200000, kategori: "Elektronik" },
+  { id: "B02", nama: "Sepeda Lipat", harga: 2800000, kategori: "Olahraga" },
+  { id: "B03", nama: "Jaket Kulit", harga: 800000, kategori: "Fashion" }
+];
