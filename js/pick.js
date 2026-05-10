1a55bd0d4f1aa8426b6bb94e5d51556cd5fdd3c5
faiznrw745-cell <faiznrw745@gmail.com>
2026-05-10 11:50:31 +0700

    menambahkan fitur transaksi

--- js/pick.js
+++ js/pick.js
@@ -4,70 +4,56 @@ const products = [
     name: "Jaket Kulit",
     price: "Rp 800.000",
     description: "Kondisi 90%, warna hitam, size L.",
-    image:
-      "https://st.depositphotos.com/3323581/54368/i/450/depositphotos_543685562-stock-photo-cool-modern-bald-man-beard.jpg"
+    image: "https://st.depositphotos.com/3323581/54368/i/450/depositphotos_543685562-stock-photo-cool-modern-bald-man-beard.jpg"
   },
-
   {
     category: "Elektronik",
     name: "Headphone Gaming",
     price: "Rp 550.000",
     description: "Lengkap box dan kabel original.",
-    image:
-      "https://c2.peakpx.com/wallpaper/45/234/482/headphones-instagram-video-games-wallpaper-preview.jpg"
+    image: "https://c2.peakpx.com/wallpaper/45/234/482/headphones-instagram-video-games-wallpaper-preview.jpg"
   },
-
   {
     category: "Aksesoris",
     name: "Jam Tangan Casio",
     price: "Rp 1.000.000",
     description: "Fungsi normal, minus pemakaian ringan.",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIbarPrJcgcliKInJ6Kok69A1c3Pu47uaTPA&s"
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIbarPrJcgcliKInJ6Kok69A1c3Pu47uaTPA&s"
   },
-
   {
     category: "Sneakers",
     name: "Sepatu Sneakers",
     price: "Rp 275.000",
     description: "Ukuran 42, kondisi sangat baik.",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvQDu4VRMSuegaUyLpIv8wK5CWT3FdOhr3bw&s"
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvQDu4VRMSuegaUyLpIv8wK5CWT3FdOhr3bw&s"
   }
 ];
 
 const productContainer = document.getElementById("productContainer");
 
-products.forEach(product => {
+// Tambahkan parameter 'index' untuk melacak barang mana yang diklik
+products.forEach((product, index) => {
   productContainer.innerHTML += `
-  
     <div class="card">
-
       <div class="card-image">
         <img src="${product.image}" alt="${product.name}">
       </div>
-
       <div class="card-content">
-
         <span class="badge">${product.category}</span>
-
         <h3>${product.name}</h3>
-
-        <div class="price-tag">
-          ${product.price}
-        </div>
-
-        <p>
-          ${product.description}
-        </p>
-
-        <button class="btn-primary">
+        <div class="price-tag">${product.price}</div>
+        <p>${product.description}</p>
+        <button class="btn-primary" onclick="simpanDanPindah(${index})">
           Pilih Barang
         </button>
-
       </div>
-
     </div>
-
   `;
 });
+
+// Fungsi untuk menyimpan data ke localStorage dan pindah halaman
+function simpanDanPindah(index) {
+  const barangDipilih = products[index];
+  localStorage.setItem('itemSwap', JSON.stringify(barangDipilih));
+  window.location.href = 'konfirm.html';
+}
