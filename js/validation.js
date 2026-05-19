f86efe70c0b603888128d07dc41a37f99dee2be5
aldoprawiroa <aldoprawiroa@gmail.com>
2026-05-19 15:15:28 +0700

    update base

--- js/validation.js
+++
@@ -1,8 +0,0 @@
-// Sistem Validasi Input oleh Faiz (018)
-function validateInput(value, fieldName) {
-    if (!value || value.trim() === '') {
-        if(typeof showToast === 'function') showToast(`${fieldName} wajib diisi terlebih dahulu!`, "error");
-        return false;
-    }
-    return true;
-}
