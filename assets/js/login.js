cc3ae7bf8e61c081d926b926110e7fa615617b1a
Abdil Fawasta Rasyadan <abdilrasya100@gmail.com>
2026-07-06 23:00:14 +0700

    feat(auth): menyiapkan kerangka modul autentikasi

---
+++ assets/js/login.js
@@ -0,0 +1,38 @@
+const STORAGE_KEY = "swapspace.login";
+const INITIAL_STATE = {
+  isReady: false,
+};
+
+const state = {
+  ...INITIAL_STATE,
+};
+
+function init() {
+  // TODO: Initialize login module.
+}
+
+function bindEvents() {
+  // TODO: Bind login form events.
+}
+
+function loadStoredState() {
+  // TODO: Load persisted state.
+}
+
+function saveStoredState() {
+  // TODO: Persist state.
+}
+
+function updateState(nextState) {
+  // TODO: Update module state.
+}
+
+function render() {
+  // TODO: Render login UI.
+}
+
+function getUtilities() {
+  // TODO: Add shared helpers.
+}
+
+document.addEventListener("DOMContentLoaded", init);
