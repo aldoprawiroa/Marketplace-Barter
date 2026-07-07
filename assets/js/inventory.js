40d13180371d8e0b306da7b3d654e19ca1d843f1
Ibra019 <him997090@gmail.com>
2026-07-07 10:43:14 +0700

    feat(inventory): menyiapkan kerangka modul inventaris

---
+++ assets/js/inventory.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // TODO: initialize inventory page
+  // TODO: render inventory items
+  // TODO: add event handlers for inventory actions
+});
